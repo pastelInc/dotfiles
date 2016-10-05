989420ad1eac8d27a8e907e8266cee2e7c445794
pastelInc <keyaki765@gmail.com>
2016-10-06 05:57:23 +0900

    fix: install using the NeoBundle

--- .vimrc
+++ .vimrc
@@ -10,6 +10,7 @@ NeoBundle 'Shougo/vimproc.vim', {
 \ 'build' : {
 \     'windows' : 'tools\\update-dll-mingw',
 \     'cygwin' : 'make -f make_cygwin.mak',
+\     'msys' : 'make -f make_cygwin.mak',
 \     'mac' : 'make',
 \     'linux' : 'make',
 \     'unix' : 'gmake',
--- etc/init/bootstrap.sh
+++ etc/init/bootstrap.sh
@@ -1,16 +1,2 @@
 # Neobundle
 [ ! -d ~/.vim/bundle ] && git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
-[ ! -d ~/.vim/bundle/vimproc.vim ] && git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim
-
-case "${OSTYPE}" in
-# Mac(Unix)
-darwin*)0
-    cd ~/.vim/bundle/vimproc.vim && make -f make_mac.mak
-    ;;  
-# Linux
-linux*)
-    cd ~/.vim/bundle/vimproc.vim && make -f make_unix.mak
-    ;;  
-esac
-
-#vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1
