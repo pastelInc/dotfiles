572cf438cdf906ec02e8338432056c45a0031aae
pastelInc <keyaki765@gmail.com>
2016-10-06 06:25:29 +0900

    feat: support to zplug

--- etc/init/bootstrap.sh
+++ etc/init/bootstrap.sh
@@ -1,3 +1,7 @@
+# zplug
+if [ ! -d ~/.zplug ]; then
+    curl -sL zplug.sh/installer | zsh
+fi
 # Neobundle
 if [ ! -d ~/.vim/bundle ]; then
     git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
