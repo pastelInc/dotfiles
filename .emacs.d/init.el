695027e6ae78a7755a36555aafa9524d8a8ea9d7
pastelInc <keyaki765@gmail.com>
2016-11-24 23:59:09 +0900

    feat: set emacs settings step by step

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,26 @@
-(require 'package)
-(package-initialize)
+; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;; ------------------------------------------------------------------------
+;; @ load-path
 
-;; cask
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
+;; load-pathの追加関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
 
-;; view
-(column-number-mode t)
-(fset 'yes-or-no-p 'y-or-n-p)
-(global-linum-mode t)
-(set-face-background 'trailing-whitespace "#b14770")
-(set-frame-parameter (selected-frame) 'alpha '(0.87))
-;(set-scroll-bar-mode nil)
-(show-paren-mode t)
-(setq frame-title-format
-      (format "%%f - Emacs@%s" (system-name)))
-(setq inhibit-startup-message t)
-(setq show-paren-delay 0)
-(setq show-paren-style 'expression)
-(setq scroll-conservatively 1)
-(setq-default tab-width 4)
-(setq-default indent-tabs-mode nil);
-(setq-default show-trailing-whitespace t)
-(setq initial-scratch-message "")
-(size-indication-mode t)
-;(tool-bar-mode -1)
+;; elisp
+;;(add-to-load-path "elisp")
 
-;; encoding
+;; ------------------------------------------------------------------------
+;; @ general
+
+;; common lisp
+(require 'cl)
+
+;; 文字コード
 (set-language-environment "Japanese")
 (let ((ws window-system))
   (cond ((eq ws 'w32)
@@ -39,6 +33,7 @@
          (prefer-coding-system 'utf-8-hfs)
          (setq file-name-coding-system 'utf-8-hfs)
          (setq locale-coding-system 'utf-8-hfs))))
+
 (let ((ws window-system))
   (cond ((eq ws 'w32)
          (set-face-attribute 'default nil
@@ -47,9 +42,199 @@
          (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Meiryo")))
         ((eq ws 'ns)
          (set-face-attribute 'default nil
-                             :family "Monaco"
+                             :family "Ricty"
                              :height 140)
-         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Monaco")))))
+         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty")))))
 
-;; key
+;; スタートアップ非表示
+(setq inhibit-startup-screen t)
+
+;; scratchの初期メッセージ消去
+(setq initial-scratch-message "")
+
+;; ツールバー非表示
+(tool-bar-mode -1)
+
+;; メニューバーを非表示
+;;(menu-bar-mode -1)
+
+;; スクロールバー非表示
+(set-scroll-bar-mode nil)
+
+;; タイトルバーにファイルのフルパス表示
+(setq frame-title-format
+      (format "%%f - Emacs@%s" (system-name)))
+
+;; 行番号表示
+(global-linum-mode t)
+(set-face-attribute 'linum nil
+                    :foreground "#800"
+                    :height 0.9)
+
+;; 行番号フォーマット
+(setq linum-format "%4d")
+
+;; 括弧の範囲内を強調表示
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'expression)
+
+;; 括弧の範囲色
+(set-face-background 'show-paren-match-face "#500")
+
+;; 選択領域の色
+(set-face-background 'region "#555")
+
+;; 行末の空白を強調表示
+(setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "#b14770")
+
+;; タブをスペースで扱う
+(setq-default indent-tabs-mode nil)
+
+;; タブ幅
+(custom-set-variables '(tab-width 4))
+
+;; yes or noをy or n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; 最近使ったファイルをメニューに表示
+(recentf-mode t)
+
+;; 最近使ったファイルの表示数
+(setq recentf-max-menu-items 10)
+
+;; 最近開いたファイルの保存数を増やす
+(setq recentf-max-saved-items 3000)
+
+;; ミニバッファの履歴を保存する
+(savehist-mode 1)
+
+;; ミニバッファの履歴の保存数を増やす
+(setq history-length 3000)
+
+;; バックアップを残さない
+(setq make-backup-files nil)
+
+;; 行間
+(setq-default line-spacing 0)
+
+;; 1行ずつスクロール
+(setq scroll-conservatively 35
+      scroll-margin 0
+      scroll-step 1)
+(setq comint-scroll-show-maximum-output t) ;; shell-mode
+
+;; フレームの透明度
+(set-frame-parameter (selected-frame) 'alpha '(0.87))
+
+;; モードラインに行番号表示
+(line-number-mode t)
+
+;; モードラインに列番号表示
+(column-number-mode t)
+
+;; C-Ret で矩形選択
+;; 詳しいキーバインド操作：http://dev.ariel-networks.com/articles/emacs/part5/
+(cua-mode t)
+(setq cua-enable-cua-keys nil)
+
+;; ------------------------------------------------------------------------
+;; @ modeline
+
+;; モードラインの割合表示を総行数表示
+(defvar my-lines-page-mode t)
+(defvar my-mode-line-format)
+
+(when my-lines-page-mode
+  (setq my-mode-line-format "%d")
+  (if size-indication-mode
+      (setq my-mode-line-format (concat my-mode-line-format " of %%I")))
+  (cond ((and (eq line-number-mode t) (eq column-number-mode t))
+         (setq my-mode-line-format (concat my-mode-line-format " (%%l,%%c)")))
+        ((eq line-number-mode t)
+         (setq my-mode-line-format (concat my-mode-line-format " L%%l")))
+        ((eq column-number-mode t)
+         (setq my-mode-line-format (concat my-mode-line-format " C%%c"))))
+
+  (setq mode-line-position
+        '(:eval (format my-mode-line-format
+                        (count-lines (point-max) (point-min))))))
+
+;; ------------------------------------------------------------------------
+;; @ initial frame maximize
+
+;; 起動時にウィンドウ最大化
+;; http://www.emacswiki.org/emacs/FullScreen#toc12
+(defun jbr-init ()
+  "Called from term-setup-hook after the default
+   terminal setup is
+   done or directly from startup if term-setup-hook not
+   used.  The value
+   0xF030 is the command for maximizing a window."
+  (interactive)
+  (w32-send-sys-command #xf030)
+  (ecb-redraw-layout)
+  (calendar))
+
+(let ((ws window-system))
+  (cond ((eq ws 'w32)
+         (set-frame-position (selected-frame) 0 0)
+         (setq term-setup-hook 'jbr-init)
+         (setq window-setup-hook 'jbr-init))
+        ((eq ws 'ns)
+         ;; for MacBook Air(Late2010) 11inch display
+         (set-frame-position (selected-frame) 0 0)
+         (set-frame-size (selected-frame) 95 47))))
+
+;; ------------------------------------------------------------------------
+;; @ key bind
+
+;; バックスラッシュ
+(define-key global-map (kbd "M-|") "\\")
+
+;; globalなC-zを無効化
+(global-unset-key "\C-z")
+
+;; ------------------------------------------------------------------------
+;; @ package manager
+
+(require 'package)
+(package-initialize)
+
+;; ------------------------------------------------------------------------
+;; @ cask
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;; ------------------------------------------------------------------------
+;; @ anything.el
+
 (define-key global-map (kbd "\C-x b") 'anything)
+
+;; ------------------------------------------------------------------------
+;; @ auto-complete.el
+
+;;(require 'auto-complete-config)
+(ac-config-default)
+;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'text-mode)
+(add-to-list 'ac-modes 'fundamental-mode)
+;;(add-to-list 'ac-modes 'org-mode)
+(ac-set-trigger-key "TAB")
+;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-menu-map t)
+;; 曖昧マッチ
+;;(setq ac-use-fuzzy t)
+
+;; ------------------------------------------------------------------------
+;; @ browse-kill-ring.el
+
+(global-set-key "\C-cy" 'browse-kill-ring)
+
+;; ------------------------------------------------------------------------
+;; @ color-theme.el
+
+;;(color-theme-initialize)
+(load-theme 'deeper-blue t)
