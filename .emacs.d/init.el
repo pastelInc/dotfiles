893a42ec32a869be11321141d99247221b3a0b66
pastelInc <keyaki765@gmail.com>
2019-07-29 20:17:01 +0900

    update default encoding settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,8 +48,16 @@
 ;;;;; Editing
 
 ;; Default Encoding
+;; reference to https://stackoverflow.com/questions/2901541/which-coding-system-should-i-use-in-emacs
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(unless (eq system-type 'windows-nt)
+  (set-selection-coding-system 'utf-8))
 (prefer-coding-system 'utf-8)
-(set-language-environment "Japanese")
 
 ;; タイトルバーにバッファーのファイルパスを表示
 (setq frame-title-format nil)
