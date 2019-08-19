ebd4a45364073821db2baaaae088328a2f8f2a4d
pastelInc <keyaki765@gmail.com>
2019-08-19 18:29:59 +0900

    add text scaling

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -162,6 +162,29 @@
 ;; "C-t"でウィンドウを切り替える
 (global-set-key (kbd "C-t") 'other-window)
 
+;; text scaling
+(define-globalized-minor-mode
+  global-text-scale-mode
+  text-scale-mode
+  (lambda () (text-scale-mode 1)))
+
+(defun global-text-scale-adjust (inc) (interactive)
+       (text-scale-set 1)
+       (kill-local-variable 'text-scale-mode-amount)
+       (setq-default text-scale-mode-amount (+ text-scale-mode-amount inc))
+       (global-text-scale-mode 1)
+       )
+
+(global-set-key (kbd "M-0")
+                '(lambda () (interactive)
+                   (global-text-scale-adjust (- text-scale-mode-amount))
+                   (global-text-scale-mode -1)))
+(global-set-key (kbd "M-+")
+                '(lambda () (interactive) (global-text-scale-adjust 1)))
+(global-set-key (kbd "M--")
+                '(lambda () (interactive) (global-text-scale-adjust -1)))
+
+
 ;;;; Search/Replace
 
 ;;;;; projectile
