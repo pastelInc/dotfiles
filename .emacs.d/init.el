dc10557dbf09fa7a2a8fcd69364dcce9d7a18641
pastelInc <keyaki765@gmail.com>
2019-12-11 03:03:03 +0900

    update settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -272,52 +272,32 @@
   :config
   (add-hook 'after-init-hook 'global-company-mode)
   :custom
-  (company-idle-delay .5)
-  ;; (company-echo-delay 0)
-  (company-minimum-prefix-length 2)
+  (company-auto-complete 'ignore)
+  (company-dabbrev-code-ignore-case t)
+  ;; (company-dabbrev-code-modes
+  ;;  '(prog-mode css-mode typescript-mode))
   (company-dabbrev-downcase nil)
+  (company-emoji-insert-unicode nil)
+  (company-flx-mode t)
+  (company-global-modes t)
+  (company-idle-delay 0)
+  ;; (company-echo-delay 0)
+  (company-lsp-cache-candidates 'auto)
+  (company-minimum-prefix-length 2)
+  (company-tooltip-align-annotations t)
+  (company-tooltip-idle-delay 0.1)
+  (company-tooltip-limit 20)
+  (company-tooltip-minimum 10)
   :defer nil
   :ensure t)
 
 (use-package company-box
   :custom
   (company-box-backends-colors nil)
+  (company-box-doc-delay 0.3)
+  (company-box-icons-alist 'company-box-icons-all-the-icons)
   (company-box-show-single-candidate t)
   (company-box-max-candidates 20)
-  :config
-  (with-eval-after-load 'all-the-icons
-      (declare-function all-the-icons-faicon 'all-the-icons)
-      (declare-function all-the-icons-fileicon 'all-the-icons)
-      (declare-function all-the-icons-material 'all-the-icons)
-      (declare-function all-the-icons-octicon 'all-the-icons)
-      (setq company-box-icons-all-the-icons
-            `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.7 :v-adjust -0.15))
-              (Text . ,(all-the-icons-faicon "book" :height 0.68 :v-adjust -0.15))
-              (Method . ,(all-the-icons-faicon "cube" :height 0.7 :v-adjust -0.05 :face 'font-lock-constant-face))
-              (Function . ,(all-the-icons-faicon "cube" :height 0.7 :v-adjust -0.05 :face 'font-lock-constant-face))
-              (Constructor . ,(all-the-icons-faicon "cube" :height 0.7 :v-adjust -0.05 :face 'font-lock-constant-face))
-              (Field . ,(all-the-icons-faicon "tags" :height 0.65 :v-adjust -0.15 :face 'font-lock-warning-face))
-              (Variable . ,(all-the-icons-faicon "tag" :height 0.7 :v-adjust -0.05 :face 'font-lock-warning-face))
-              (Class . ,(all-the-icons-faicon "clone" :height 0.65 :v-adjust 0.01 :face 'font-lock-constant-face))
-              (Interface . ,(all-the-icons-faicon "clone" :height 0.65 :v-adjust 0.01))
-              (Module . ,(all-the-icons-octicon "package" :height 0.7 :v-adjust -0.15))
-              (Property . ,(all-the-icons-octicon "package" :height 0.7 :v-adjust -0.05 :face 'font-lock-warning-face)) ; Golang module
-              (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.7 :v-adjust -0.15))
-              (Value . ,(all-the-icons-material "format_align_right" :height 0.7 :v-adjust -0.15 :face 'font-lock-constant-face))
-              (Enum . ,(all-the-icons-material "storage" :height 0.7 :v-adjust -0.15 :face 'all-the-icons-orange))
-              (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.7 :v-adjust -0.15))
-              (Snippet . ,(all-the-icons-faicon "code" :height 0.7 :v-adjust 0.02 :face 'font-lock-variable-name-face))
-              (Color . ,(all-the-icons-material "palette" :height 0.7 :v-adjust -0.15))
-              (File . ,(all-the-icons-faicon "file-o" :height 0.7 :v-adjust -0.05))
-              (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.7 :v-adjust -0.15))
-              (Folder . ,(all-the-icons-octicon "file-directory" :height 0.7 :v-adjust -0.05))
-              (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.7 :v-adjust -0.15 :face 'all-the-icons-blueb))
-              (Constant . ,(all-the-icons-faicon "tag" :height 0.7 :v-adjust -0.05))
-              (Struct . ,(all-the-icons-faicon "clone" :height 0.65 :v-adjust 0.01 :face 'font-lock-constant-face))
-              (Event . ,(all-the-icons-faicon "bolt" :height 0.7 :v-adjust -0.05 :face 'all-the-icons-orange))
-              (Operator . ,(all-the-icons-fileicon "typedoc" :height 0.65 :v-adjust 0.05))
-              (TypeParameter . ,(all-the-icons-faicon "hashtag" :height 0.65 :v-adjust 0.07 :face 'font-lock-const-face))
-              (Template . ,(all-the-icons-faicon "code" :height 0.7 :v-adjust 0.02 :face 'font-lock-variable-name-face)))))
   :ensure t
   :hook (company-mode . company-box-mode))
 
@@ -393,6 +373,7 @@
   (lsp-elm-elm-path (my/file-exists-fallback "./node_modules/.bin/elm" "elm"))
   (lsp-elm-elm-format-path (my/file-exists-fallback "./node_modules/.bin/elm-format" "elm-format"))
   (lsp-elm-elm-test-path (my/file-exists-fallback "./node_modules/.bin/elm-test" "elm-test"))
+  (lsp-elm-elm-analyse-trigger "never")
   :ensure t
   :hook ((web-mode scss-mode typescript-mode go-mode python-mode elm-mode) . lsp)
   :preface
@@ -519,9 +500,9 @@
 
 ;;;;; Elm
 (use-package elm-mode
+  :custom
+  (elm-format-on-save nil)
   :ensure t
-  :init
-  (setq elm-format-on-save t)
   :mode ("\\.elm\\'" . elm-mode))
 
 ;;;;; Go
