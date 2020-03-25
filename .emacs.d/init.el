f8d0139ba5dc298a0a8dbf625e1d2bf9e9598baf
pastelInc <keyaki765@gmail.com>
2020-03-25 22:37:09 +0900

    update indent configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -467,6 +467,7 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'web-mode-comment-formats '("javascript" . "//"))
+  (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
   :custom
   (web-mode-markup-indent-offset 2) ; HTMLのインデイント
   (web-mode-css-indent-offset 2) ; CSSのインデント
